df1f19151411b50e3647892207ea4b01bbd0595f
mintu-1996 <abhaybansal999@gmail.com>
2017-08-02 17:01:20 +0530

    style linear

--- app/src/main/java/com/example/mintu_1996/project/register_activity.java
+++ app/src/main/java/com/example/mintu_1996/project/register_activity.java
@@ -3,14 +3,12 @@ package com.example.mintu_1996.project;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class register_activity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toast.makeText(this, "done YOOO!!!!!!", Toast.LENGTH_SHORT).show();
+        setContentView(R.layout.activity_register_activity);
     }
 }
