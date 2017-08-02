116abe2d9b783c619a248c879267721378762afc
cheekushivam <sg5973676@gmail.com>
2017-08-02 12:10:30 +0530

    done commit2

--- app/src/main/java/com/example/mintu_1996/project/MainActivity.java
+++ app/src/main/java/com/example/mintu_1996/project/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "done!!!!!!n!", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "done!!!!!!n11!", Toast.LENGTH_SHORT).show();
     }
 }
